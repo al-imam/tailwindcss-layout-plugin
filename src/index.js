2471a02b9f2e545bbd2620cac0e2fc8033cff0a9
al-imam <alimam01828@gmail.com>
2023-11-24 15:11:53 +0600

    Fix: Resolve dangerous bug

--- src/index.js
+++ src/index.js
@@ -1,56 +1,68 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin")
 
-const content = plugin.withOptions(function (options = { strategy: undefined }) {
+const content = plugin.withOptions(function (options = { strategy: "auto" }) {
   return function ({ addBase, addComponents, theme }) {
     const rules = [
       {
-        '.content-grid': {
-          '--padding-inline': '1rem',
-          '--content-max-width': '900px',
-          '--breakout-max-width': '1200px',
+        ".content": {
+          "--content-max-width": "900px",
+          "--padding-inline": "1rem",
+          "--popout-extra-width": "2rem",
+          "--feature-extra-width": "2rem",
 
-          '--breakout-size': `calc(
-            (var(--breakout-max-width) - var(--content-max-width)) / 2
-          )`,
+          "--gap": "var(--padding-inline, 1rem)",
+          "--full": "minmax(var(--gap), 1fr)",
+          "--content": "min(var(--content-max-width, 70ch), 100% - var(--gap) * 2);",
+          "--popout": "minmax(0, var(--popout-extra-width, 2rem))",
+          "--feature": "minmax(0, var(--feature-extra-width, 5rem))",
 
-          display: 'grid',
+          display: "grid",
+          columnGap: "0 !important",
           gridTemplateColumns: `
-            [full-width-start] 
-              minmax(var(--padding-inline), 1fr)
-                [breakout-start] 
-                  minmax(0, var(--breakout-size))
-                    [content-start] 
-                      min(100% - (var(--padding-inline) * 2),var(--content-max-width))
-                    [content-end]
-                  minmax(0, var(--breakout-size)) 
-                [breakout-end]
-              minmax(var(--padding-inline), 1fr)
-            [full-width-end]
+            [full-start] 
+              var(--full)
+                [feature-start] 
+                  var(--feature)
+                    [popout-start] 
+                      var(--popout)
+                        [content-start] 
+                          var(--content) 
+                        [content-end]
+                      var(--popout) 
+                    [popout-end]
+                  var(--feature) 
+                [feature-end]
+              var(--full) 
+            [full-end]
           `,
         },
       },
 
       {
         [[
-          '.content-grid > :not(.breakout, .full-width)',
-          '.full-width > :not(.breakout, .full-width)',
+          ".content > :not(.content-popout, .content-feature, .content-full-width, .content-full-width-grow)",
+          ".content-full-width > :not(.content-popout, .content-feature, .content-full-width, .content-full-width-grow)",
         ]]: {
-          gridColumn: 'content',
+          gridColumn: "content",
         },
-      },
 
-      {
-        '.content-grid > .breakout': {
-          gridColumn: 'breakout',
+        ".content > .content-popout": {
+          gridColumn: "popout",
         },
-      },
 
-      {
-        '.content-grid > .full-width': {
-          gridColumn: 'full-width',
+        ".content > .content-feature": {
+          gridColumn: "feature",
+        },
 
-          display: 'grid',
-          gridTemplateColumns: 'inherit',
+        ".content > .content-full-width": {
+          gridColumn: "full",
+          columnGap: "0 !important",
+          display: "grid",
+          gridTemplateColumns: "inherit",
+        },
+
+        ".content > .content-full-width-grow": {
+          gridColumn: "full",
         },
       },
     ]
