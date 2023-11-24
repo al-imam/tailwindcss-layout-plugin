caee6bd08acf9d038eba7bf8b4251efa2ca5318f
al-imam <alimam01828@gmail.com>
2023-11-24 18:45:10 +0600

    add typescript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,23 @@
-declare function plugin(options?: Partial<{ strategy: 'container' | 'auto' }>): {
+type CSSValue = `${number}px` | `${number}em` | `${number}rem` | `${number}%` | `var(--${string})`
+
+interface Screens {
+  DEFAULT: CSSValue
+  sm: CSSValue
+  md: CSSValue
+  lg: CSSValue
+  xl: CSSValue
+  "2xl": CSSValue
+}
+
+declare function plugin(
+  options?: Partial<{
+    strategy: "container" | "auto"
+    content: Screens | CSSValue
+    popout: Screens | CSSValue
+    feature: Screens | CSSValue
+    gap: Screens | CSSValue
+  }>
+): {
   handler: () => void
 }
 
