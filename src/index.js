932599cbe764f1bc1ba221a01216286b042cfe2e
al-imam <alimam01828@gmail.com>
2023-11-24 18:45:01 +0600

    make it configurable and extends default options

--- src/index.js
+++ src/index.js
@@ -1,24 +1,110 @@
 const plugin = require("tailwindcss/plugin")
 
-const content = plugin.withOptions(function (options = { strategy: "auto" }) {
+function responsiveValues({ gap, feature, popout, maxWidth, content, strategy }) {
+  const apply = strategy === "container"
+
+  return {
+    "@media (min-width: 640px)": {
+      "--padding-inline": gap.sm,
+      "--popout-extra-width": feature.sm,
+      "--feature-extra-width": popout.sm,
+      "--content-max-width": apply ? content.sm : maxWidth,
+    },
+
+    "@media (min-width: 768px)": {
+      "--padding-inline": gap.md,
+      "--popout-extra-width": feature.md,
+      "--feature-extra-width": popout.md,
+      "--content-max-width": apply ? content.md : maxWidth,
+    },
+    "@media (min-width: 1024px)": {
+      "--padding-inline": gap.lg,
+      "--popout-extra-width": feature.lg,
+      "--feature-extra-width": popout.lg,
+      "--content-max-width": apply ? content.lg : maxWidth,
+    },
+
+    "@media (min-width: 1280px)": {
+      "--padding-inline": gap.xl,
+      "--popout-extra-width": feature.xl,
+      "--feature-extra-width": popout.xl,
+      "--content-max-width": apply ? content.lg : maxWidth,
+    },
+
+    "@media (min-width: 1536px)": {
+      "--padding-inline": gap["2xl"],
+      "--popout-extra-width": feature["2xl"],
+      "--feature-extra-width": popout["2xl"],
+      "--content-max-width": apply ? content["2xl"] : maxWidth,
+    },
+  }
+}
+
+function mapScreenSizes(value) {
+  return { DEFAULT: value, md: value, sm: value, lg: value, xl: value, "2xl": value }
+}
+
+function getScreenSizes(gap) {
+  if (typeof gap === "string") return mapScreenSizes(gap)
+  if (typeof gap === "object") {
+    for (const key in gap) {
+      if (typeof gap[key] !== "string") {
+        throw new Error("Gap value is not valid")
+      }
+    }
+
+    const computedGap = {}
+
+    computedGap.DEFAULT = gap.DEFAULT ?? "1rem"
+    computedGap.sm = gap.sm ?? computedGap.DEFAULT
+    computedGap.md = gap.md ?? computedGap.sm
+    computedGap.lg = gap.lg ?? computedGap.md
+    computedGap.xl = gap.xl ?? computedGap.lg
+    computedGap["2xl"] = gap["2xl"] ?? computedGap.xl
+
+    return computedGap
+  }
+
+  throw new Error("Gap value is not valid")
+}
+
+const content = plugin.withOptions(function (options = {}) {
   return function ({ addBase, addComponents, theme }) {
+    options.strategy ??= "auto"
+    options.gap = getScreenSizes(options.gap ?? theme("container.padding") ?? "1rem")
+    options.maxWidth ??= "56.25rem"
+    options.content = getScreenSizes(
+      options.content ?? {
+        DEFAULT: "100%",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      }
+    )
+    options.popout = getScreenSizes(options.popout ?? "2rem")
+    options.feature = getScreenSizes(options.feature ?? "5rem")
+
     const rules = [
       {
-        ".content": {
-          "--content-max-width": "900px",
-          "--padding-inline": "1rem",
-          "--popout-extra-width": "2rem",
-          "--feature-extra-width": "2rem",
+        ".content": Object.assign(
+          {
+            "--content-max-width":
+              options.strategy === "auto" ? options.maxWidth : options.content.DEFAULT,
+            "--padding-inline": options.gap.DEFAULT,
+            "--popout-extra-width": options.popout.DEFAULT,
+            "--feature-extra-width": options.feature.DEFAULT,
 
-          "--gap": "var(--padding-inline, 1rem)",
-          "--full": "minmax(var(--gap), 1fr)",
-          "--content": "min(var(--content-max-width, 70ch), 100% - var(--gap) * 2);",
-          "--popout": "minmax(0, var(--popout-extra-width, 2rem))",
-          "--feature": "minmax(0, var(--feature-extra-width, 5rem))",
+            "--gap": "var(--padding-inline, 1rem)",
+            "--full": "minmax(var(--gap), 1fr)",
+            "--content": "min(var(--content-max-width, 70ch), 100% - var(--gap) * 2);",
+            "--popout": "minmax(0, var(--popout-extra-width, 2rem))",
+            "--feature": "minmax(0, var(--feature-extra-width, 5rem))",
 
-          display: "grid",
-          columnGap: "0 !important",
-          gridTemplateColumns: `
+            display: "grid",
+            columnGap: "0 !important",
+            gridTemplateColumns: `
             [full-start] 
               var(--full)
                 [feature-start] 
@@ -35,7 +121,9 @@ const content = plugin.withOptions(function (options = { strategy: "auto" }) {
               var(--full) 
             [full-end]
           `,
-        },
+          },
+          responsiveValues(options)
+        ),
       },
 
       {
